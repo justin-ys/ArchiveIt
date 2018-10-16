c216131b9778c86638b2d4a6f737c71bbad03018
Jman005 <n/a>
2018-10-15 21:32:23 -0400

    Barebones HTML Parser Recursive comment parser now also returns a list instead of a string

--- src/libformatter.py
+++ src/libformatter.py
@@ -1,5 +1,7 @@
 import praw
 import re
+import config
+from Cheetah.Template import Template
 
 LIBVER = "Alpha1.0"
 
@@ -25,6 +27,21 @@ class PostFormatter():
     def get_comments(self):
         raise NotImplementedError()
 
+    def parse_comment(self, comments, lvl=0):
+        li = []
+        for cmt in comments:
+            li.append((cmt, lvl))
+            # We add an indent to the comment string relative to its reply depth,
+            # add the same indent to any newlines in the string and append it
+            # to our return var
+            if cmt.replies is not None:
+                li.extend(self.parse_comment(cmt.replies, lvl+1))
+
+        return li
+
+    def format_comment(self, comment, lvl):
+        raise NotImplementedError()
+
     def out(self):
         raise NotImplementedError()
 
@@ -35,6 +52,7 @@ class TextFormatter(PostFormatter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.post.comments.replace_more(limit=None)
+        self.filetype = ".txt"
 
     def get_author(self):
         return self.post.author.name if self.post.author is not None else "[deleted]"
@@ -50,40 +68,23 @@ class TextFormatter(PostFormatter):
             return "---------\n[deleted]\n---------"
 
     def get_comments(self):
-        """Ugly recursive function...
-        but it does the job. Loops
-        through a CommentForest,
-        checks if the comment has
-        replies and if so calls the function
-        on said reply."""
 
-        def formatcmt(comment, lvl):
-            # NASA has extensively researched and concluded there is no cleaner way to do this.
-            stri = (
-                    "\n\n%s" % ("\t" * lvl) +
-                    (comment.author.name if comment.author is not None else "[deleted]") +
-                    " | " +
-                    str(comment.score if comment.score is not None else "[deleted]") +
-                    " points" +
-                    " | posted on " +
-                    str(comment.created_utc if comment.created_utc is not None else "[deleted]") +
-                    ("\n[" + comment.body + "]").replace("\n", "\n%s" % ("\t" * lvl))
-            )
-            return stri
+        comments = self.parse_comment(self.post.comments)
+        return "".join([self.format_comment(cmt[0], cmt[1]) for cmt in comments])
 
-        def parse(comments, lvl=0):
-            stri = ""
-            for cmt in comments:
-                # We add an indent to the comment string relative to its reply depth,
-                # add the same indent to any newlines in the string and append it
-                # to our return var
-                stri += formatcmt(cmt, lvl)
-                if cmt.replies != None:
-                    stri += parse(cmt.replies, lvl + 1)
-
-            return stri
-
-        return parse(self.post.comments)
+    def format_comment(self, comment, lvl):
+        # NASA has extensively researched and concluded there is no cleaner way to do this.
+        stri = (
+                "\n\n%s" % ("\t" * lvl) +
+                (comment.author.name if comment.author is not None else "[deleted]") +
+                " | " +
+                str(comment.score if comment.score is not None else "[deleted]") +
+                " points" +
+                " | posted on " +
+                str(comment.created_utc if comment.created_utc is not None else "[deleted]") +
+                ("\n[" + comment.body + "]").replace("\n", "\n%s" % ("\t" * lvl))
+        )
+        return stri
 
     def out(self):
         return (
@@ -103,8 +104,56 @@ class TextFormatter(PostFormatter):
         )
 
 
+class HTMLFormatter(PostFormatter):
+    def __init__(self, *args, **kwargs):
+        super(HTMLFormatter, self).__init__(*args, **kwargs)
+        self.filetype = ".html"
+
+    def get_author(self):
+        return "<p>$post.author.name\n</p>" if self.post.author is not None else "[deleted]"
+
+    def get_selftext(self):
+        return "<p>$post.selftext\n</p>" if self.post.is_self else "\n\n"
+
+    def get_comments(self):
+        return """#for $comment in $parsed_comments
+        <p>$#echo $format_comment($comment[0], 0)# </p>
+        
+        #end for"""
+
+    def format_comment(self, comment, lvl):
+        print(comment)
+        return comment.body
+
+    def out(self):
+        print(self.parse_comment(self.post.comments))
+        template = self.get_author() + self.get_selftext() + self.get_comments()
+        return Template(template,
+                        searchList=[
+                            {'format_comment': self.format_comment,
+                             'post': self.post,
+                             'parsed_comments': self.parse_comment(self.post.comments)}
+                        ]
+                        )
+
+
 def get_format(stri):
     if re.match(".?[Tt]ext.?", stri) is not None:
         return TextFormatter
 
+    elif re.match(".?[Hh]tml.?", stri) is not None:
+        return HTMLFormatter
+
     return None
+
+
+reddit = praw.Reddit(user_agent=config.get_useragent(),
+                     username=config.get_username(),
+                     password=config.get_password(),
+                     client_id=config.get_clientid(),
+                     client_secret=config.get_clientsecret()
+                     )
+
+subm = reddit.submission('9nut40')
+print(list(filter(None, HTMLFormatter(subm).parse_comment(subm.comments))))
+print(HTMLFormatter(subm).out())
